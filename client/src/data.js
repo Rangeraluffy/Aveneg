43735eeef4cec7702c654e880f200ffa34701f53
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-24 16:12:42 +0100

    Create slider React

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdn.discordapp.com/attachments/286902629643321344/851821452856262656/BASIC_DOS_LIN_BEIGE_-_copie_1080x-Nike-Man.jpg",
+      title: "SUMMER SALE",
+      desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://cdn.discordapp.com/attachments/286902629643321344/851821576433172510/test_tuto_2.png",
+      title: "POPULAR SALE",
+      desc: "GET FLAT 70% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://cdn.discordapp.com/attachments/286902629643321344/851821452856262656/BASIC_DOS_LIN_BEIGE_-_copie_1080x-Nike-Man.jpg",
+        title: "SUMMER SALE",
+        desc: "GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+      },
+  ];
