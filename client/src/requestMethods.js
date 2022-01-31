40e1111da74c280f4ba5693b4f8b02e07ddd36d9
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-31 15:40:23 +0100

    Create Admin react part

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
-
-const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
+const TOKEN = "00000";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
